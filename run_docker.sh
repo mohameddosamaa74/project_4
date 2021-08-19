6f566e10e8a17908801e0bdc73ee74d37ce869e3
Mohamed Osama <76261253+mohameddosamaa74@users.noreply.github.com>
2021-08-19 15:41:11 +0200

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -2,10 +2,10 @@
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
-docker build --tag=mohameddosamaa225/project_4 
+docker build --tag=project_4 
 
 # Step 2: 
 docker image ls
 
 # Step 3: 
-docker run -p 8000:80 mohameddosamaa225/project_4
+docker run -p 8000:80 project_4
